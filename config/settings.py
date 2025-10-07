f801dcac00c111d791786261781baed071c98eb3
César Berbel <cesarberbelqa@gmail.com>
2025-10-07 16:49:20 +0100

    Refactor templates to Bootstrap 5 + django-crispy-forms (bootstrap5); Bootstrap toasts for messages

--- config/settings.py
+++ config/settings.py
@@ -37,10 +37,16 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'crispy_forms',
+    'crispy_bootstrap5',
+
     'core',
     'accounts',
 ]
 
+# Crispy configuration (use Bootstrap 5)
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
